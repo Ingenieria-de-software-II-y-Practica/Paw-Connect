e11aa1d2c7a30c59ca91c95af52d2176e5b0f03b
Tomás Varas Oviedo <88008465+TomyVarasOviedo@users.noreply.github.com>
2024-04-26 18:33:44 -0300

    arreglo del main

--- src/Backend/Models/Refugio.java
+++ src/Backend/Models/Refugio.java
@@ -1,5 +1,59 @@
 package Backend.Models;
 
-public class Refugio{
+import java.nio.file.Path;
+/**
+ * Clase refugio hereda de Usuario porque es un tipo de usuario.
+ */
+public class Refugio extends Usuario{
+    /*
+     * Atributos
+     */
+    private String direccion, tipo_mascota;
+    private Path foto;
+    /*
+     * Metodos
+     */
 
+    /**
+     * Constructor que amplia el constructor de usuario con los atributos de esta clase.
+     * @param id
+     * @param nombre
+     * @param contraseña
+     * @param numero_contacto
+     * @param direccion
+     * @param tipo_mascota
+     * @param foto
+     */
+    public Refugio(int id, String nombre, String contraseña, String numero_contacto, String direccion, String tipo_mascota, Path foto) {
+        super(id, nombre, contraseña, numero_contacto);
+        setAcceso(true); // True indica que el usuario es un refugio.
+        this.direccion = direccion;
+        this.tipo_mascota = tipo_mascota;
+        this.foto = foto;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTipo_mascota() {
+        return tipo_mascota;
+    }
+
+    public void setTipo_mascota(String tipo_mascota) {
+        this.tipo_mascota = tipo_mascota;
+    }
+
+    public Path getFoto() {
+        return foto;
+    }
+
+    public void setFoto(Path foto) {
+        this.foto = foto;
+    }
+    
 }
