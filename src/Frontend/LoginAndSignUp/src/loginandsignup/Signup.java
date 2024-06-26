99db39a72bde3fef3353ae2f8e56df3811dfe78b
Tomás Varas Oviedo <88008465+TomyVarasOviedo@users.noreply.github.com>
2024-06-26 19:45:09 -0300

    Login

--- src/Frontend/LoginAndSignUp/src/loginandsignup/Signup.java
+++ src/Frontend/LoginAndSignUp/src/loginandsignup/Signup.java
@@ -238,7 +238,11 @@ public class Signup extends javax.swing.JFrame {
     }//GEN-LAST:event_jTextField1ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        Menu MenuFrame = new Menu();
+        MenuFrame.setVisible(true);
+        MenuFrame.pack();
+        MenuFrame.setLocationRelativeTo(null);
+        this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
