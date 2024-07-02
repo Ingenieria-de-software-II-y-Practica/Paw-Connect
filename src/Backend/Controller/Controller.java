b580f082d331be398aff8f9afd405cf02ff8dafa
Beltrameme <53550029+Beltrameme@users.noreply.github.com>
2024-07-02 20:38:27 -0300

    ahora anda la db
    habia que instanciarla lol

--- src/Backend/Controller/Controller.java
+++ src/Backend/Controller/Controller.java
@@ -5,6 +5,7 @@ import Backend.DB.Exceptions.UserDoesNotExistException;
 
 public class Controller {
     public static void main(String[] args) throws UserDoesNotExistException {
+        DB db = new DB();
         System.out.println(DB.existeUsuario("algo"));
     }
     
